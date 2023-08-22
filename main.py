e8543b086386ce4dcfef4c808a8e6d0ef1840e63
Vr61v <nikita20040503@gmail.com>
2023-08-22 23:25:47 +0300

    Excel completed

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import fitz
 from openpyxl import load_workbook
 import openpyxl.worksheet.worksheet
+from openpyxl.styles import Alignment
+
 from findFunctions import findRegion, findPlace, findLocality, findRequestDate, findConsiderationDate
 
-count = int(input("Количество pdf файлов: "))
 
-excel = openpyxl.load_workbook(r"26.05.2023 Реестр по регистрационной работе.xlsx")
-page = excel['Реестр']
-numberRequest = int(page[f'A{page.max_row}'].internal_value.split('/')[0]) + 1
+def countRequest():
+    excel = openpyxl.load_workbook(r"26.05.2023 Реестр по регистрационной работе.xlsx")
+    page = excel['Реестр']
+    return int(page[f'A{page.max_row}'].internal_value.split('/')[0]) + 1
+
+
+count = int(input("Количество pdf файлов: "))
+numberRequest = countRequest()
 
 for iterator in range(1, count + 1):
     fileName = f"{iterator}.pdf"
@@ -19,19 +25,20 @@ for iterator in range(1, count + 1):
     region = findRegion(address)
     locality = findLocality(address)
     action = "Выписка из ЕГРН"
-    requestDate = findRequestDate(text)
-    considerationDate = findConsiderationDate(text)
+    date = findRequestDate(text)
 
-    excel2 = openpyxl.load_workbook(r"26.05.2023 Реестр по регистрационной работе.xlsx")
-    page2 = excel2['Реестр']
-    max_row = page2.max_row + 1
-    page2[f'A{max_row}'] = f"{numberRequest}/{iterator}"
-    page2[f'B{max_row}'] = f"{region}"
-    page2[f'C{max_row}'] = f"{locality}"
-    page2[f'D{max_row}'] = f"{address}"
-    page2[f'F{max_row}'] = f"{action}"
-    page2[f'G{max_row}'] = f"{requestDate}"
-    page2[f'H{max_row}'] = f"{requestDate}"
-    page2[f'I{max_row}'] = f"{considerationDate}"
+    excel = openpyxl.load_workbook(r"26.05.2023 Реестр по регистрационной работе.xlsx")
+    page = excel['Реестр']
+    max_row = page.max_row + 1
 
-    excel2.save(r"26.05.2023 Реестр по регистрационной работе.xlsx")
+    page.append({'A': f"{numberRequest}/{iterator}", 'B': f"{region}", 'C': f"{locality}",
+                 'D': f"{address}", 'E': f"{action}", 'G': f"{date}", 'H': f"{date}",
+                 'I': f"{date}", 'N': f"{date}"})
+
+    for letter in ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R']:
+        page[f'{letter}{max_row}'].alignment = Alignment(wrapText=True)
+    for letter in ['G','H','I','N']:
+        page[f'{letter}{max_row}'].number_format = openpyxl.styles.numbers.BUILTIN_FORMATS[15]
+        page[f'{letter}{max_row}'].alignment = Alignment(horizontal="right")
+
+    excel.save(r"26.05.2023 Реестр по регистрационной работе.xlsx")
