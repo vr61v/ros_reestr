468fa5f334546918f0827050a0904adf780d933d
Vr61v <nikita20040503@gmail.com>
2023-08-24 18:02:11 +0300

    Create func. appendInExcel

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import fitz
 import openpyxl.worksheet.worksheet
 from openpyxl.styles import Alignment
-
-from findFunctions import findRegion, findPlace, findLocality, findRequestDate, findConsiderationDate
+from findFunctions import findRegion, findPlace, findLocality, findRequestDate
 
 
 def countRequest():
     excel = openpyxl.load_workbook(r"26.05.2023 Реестр по регистрационной работе.xlsx")
     page = excel['Реестр']
     return int(page[f'A{page.max_row}'].internal_value.split('/')[0]) + 1
+
+
+def appendInExcel(numberRequest, iterator, region, locality, address, action, date):
+    page = excel['Реестр']
+    max_row = page.max_row + 1
+    page.append({'A': f"{numberRequest}/{iterator}", 'B': f"{region}", 'C': f"{locality}",
+                 'D': f"{address}", 'E': f"{action}", 'G': f"{date}", 'H': f"{date}",
+                 'I': f"{date}", 'N': f"{date}"})
+
+    for letter in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R']:
+        page[f'{letter}{max_row}'].alignment = Alignment(wrapText=True)
+    for letter in ['G', 'H', 'I', 'N']:
+        page[f'{letter}{max_row}'].number_format = openpyxl.styles.numbers.BUILTIN_FORMATS[15]
+        page[f'{letter}{max_row}'].alignment = Alignment(horizontal="right")
 
 
 count = int(input("Количество pdf файлов: "))
@@ -28,17 +41,6 @@ for iterator in range(1, count + 1):
     action = "Выписка из ЕГРН"
     date = findRequestDate(text)
 
-    page = excel['Реестр']
-    max_row = page.max_row + 1
-
-    page.append({'A': f"{numberRequest}/{iterator}", 'B': f"{region}", 'C': f"{locality}",
-                 'D': f"{address}", 'E': f"{action}", 'G': f"{date}", 'H': f"{date}",
-                 'I': f"{date}", 'N': f"{date}"})
-
-    for letter in ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R']:
-        page[f'{letter}{max_row}'].alignment = Alignment(wrapText=True)
-    for letter in ['G','H','I','N']:
-        page[f'{letter}{max_row}'].number_format = openpyxl.styles.numbers.BUILTIN_FORMATS[15]
-        page[f'{letter}{max_row}'].alignment = Alignment(horizontal="right")
+    appendInExcel(numberRequest, iterator, region, locality, address, action, date)
 
 excel.save(r"26.05.2023 Реестр по регистрационной работе.xlsx")
